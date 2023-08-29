cf1f252ebe2325e9ce734a883b8a884d32c86c18
CHARM <CHARM@172.22.16.79>
2023-08-29 16:14:13 +0800

    1st Java Project

---
+++ src/cs4_charm/tagorda/CS4_CharmTagorda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4_charm.tagorda;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4_CharmTagorda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String objA = "Driver 1";
+        String objB = "Driver 2";
+        String objC = "Driver 3";
+        System.out.println(objA + "\n" + objB + "\n" + objC);
+    }
+    
+}
